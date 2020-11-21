c1482f732e2b5daa75a737125c73df7cbdf3a31e
tranhuucanh2000 <18520514@gm.uit.edu.vn>
2020-11-21 11:27:51 +0700

    Hoan thien cac chuc nang sau thay doi csdl

--- QuanLyNhaSach/QuanLyNhaSach/DAO/SachDAO.cs
+++ QuanLyNhaSach/QuanLyNhaSach/DAO/SachDAO.cs
@@ -23,43 +23,43 @@ namespace QuanLyNhaSach.DAO
         }
         public DataTable TimSachQuaTen(string tenSach)
         {
-            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSachQuaTenSach @tenSach", new object[] { tenSach });   
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSach_TenSach @tenSach", new object[] { tenSach });   
             return data;
         }
         public DataTable TimSachQuaTheLoai(string theLoai)
         {
-            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSachQuaTheLoai @theLoai", new object[] { theLoai });
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSach_TheLoai @theLoai", new object[] { theLoai });
             return data;
         }
         public DataTable TimSachQuaTacGia(string tacGia)
         {
-            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSachQuaTacGia @tacGia", new object[] { tacGia });
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSach_TacGia @tacGia", new object[] { tacGia });
             return data;
         }
 
         public DataTable TimSachQuaTenVaTacGia(string ten, string tacGia)
         {
-            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSachQuaTenVaTacGia @tenSach , @tacGia", new object[] { ten , tacGia});
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSach_TenSach_TacGia @tenSach , @tacGia", new object[] { ten , tacGia});
             return data;
         }
         public DataTable TimSachQuaTenVaTheLoai(string ten, string theLoai)
         {
-            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSachQuaTenVaTheloai @tenSach , @theLoai", new object[] { ten,theLoai });
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSach_TenSach_TheLoai @tenSach , @theLoai", new object[] { ten,theLoai });
             return data;
         }
         public DataTable TimSachQuaTheLoaiVaTacGia(string theLoai, string tacGia)
         {
-            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSachQuaTheLoaiVaTacGia @tacGia , @theLoai", new object[] { tacGia,theLoai });
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSach_TacGia_TheLoai @tacGia , @theLoai", new object[] { tacGia,theLoai });
             return data;
         }
         public DataTable TimSachQuaTenVaTheLoaiVaTacGia(string ten, string theLoai, string tacGia)
         {
-            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSachQuaTenVaTheLoaiVaTacGia @tacGia , @theLoai , @tenSach", new object[] { tacGia,theLoai,ten });
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_TimSach_TenSach_TacGia_TheLoai @tenSach , @tacGia , @theLoai", new object[] { ten,tacGia,theLoai });
             return data;
         }
-        public void ThanhToanSach(string tenSach, string soLuong)
+        public void ThanhToanSach(string maSach, string soLuong)
         {
-            DataProvider.Instance.ExecuteQuery("EXEC USN_ThanhToanSach @tenSach , @soLuong", new object[] { tenSach, int.Parse(soLuong) });
+            DataProvider.Instance.ExecuteQuery("EXEC USP_ThanhToanSach @maSach , @soLuong", new object[] { maSach, int.Parse(soLuong) });
         }
     }
 }
