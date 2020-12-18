90ce1d703414266e384852702a6e1f047f6a540d
tranhuucanh2000 <18520514@gm.uit.edu.vn>
2020-12-19 02:10:09 +0700

    Hoan thanh chuc nang cai dat

--- QuanLyNhaSach/QuanLyNhaSach/UCCaiDat.Designer.cs
+++ QuanLyNhaSach/QuanLyNhaSach/UCCaiDat.Designer.cs
@@ -41,8 +41,6 @@ namespace QuanLyNhaSach
             this.ckbNhapSach = new System.Windows.Forms.CheckBox();
             this.ckbBanSach = new System.Windows.Forms.CheckBox();
             this.ckbThongKe = new System.Windows.Forms.CheckBox();
-            this.pnMKM = new System.Windows.Forms.Panel();
-            this.txbMatKhauMoi = new System.Windows.Forms.TextBox();
             this.pnNL = new System.Windows.Forms.Panel();
             this.txbNhapLaiMKM = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
@@ -52,7 +50,6 @@ namespace QuanLyNhaSach
             this.panel8 = new System.Windows.Forms.Panel();
             this.txbTenDN = new System.Windows.Forms.TextBox();
             this.lbNL = new System.Windows.Forms.Label();
-            this.lbMKM = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.lbTenSach = new System.Windows.Forms.Label();
@@ -63,12 +60,14 @@ namespace QuanLyNhaSach
             this.lbHoTro = new System.Windows.Forms.Label();
             this.pbDangXuat = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pnMK = new System.Windows.Forms.Panel();
             this.panel2.SuspendLayout();
             this.panel4.SuspendLayout();
             this.grbThongTinTaiKhoan.SuspendLayout();
             this.grbPhanQuyen.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbDangXuat)).BeginInit();
             this.panel1.SuspendLayout();
+            this.pnMK.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
@@ -105,20 +104,12 @@ namespace QuanLyNhaSach
             // 
             // grbThongTinTaiKhoan
             // 
+            this.grbThongTinTaiKhoan.Controls.Add(this.pnMK);
             this.grbThongTinTaiKhoan.Controls.Add(this.grbPhanQuyen);
-            this.grbThongTinTaiKhoan.Controls.Add(this.pnMKM);
-            this.grbThongTinTaiKhoan.Controls.Add(this.txbMatKhauMoi);
-            this.grbThongTinTaiKhoan.Controls.Add(this.pnNL);
-            this.grbThongTinTaiKhoan.Controls.Add(this.txbNhapLaiMKM);
-            this.grbThongTinTaiKhoan.Controls.Add(this.panel3);
             this.grbThongTinTaiKhoan.Controls.Add(this.panel7);
-            this.grbThongTinTaiKhoan.Controls.Add(this.txbMatKhau);
             this.grbThongTinTaiKhoan.Controls.Add(this.txbTenHT);
             this.grbThongTinTaiKhoan.Controls.Add(this.panel8);
             this.grbThongTinTaiKhoan.Controls.Add(this.txbTenDN);
-            this.grbThongTinTaiKhoan.Controls.Add(this.lbNL);
-            this.grbThongTinTaiKhoan.Controls.Add(this.lbMKM);
-            this.grbThongTinTaiKhoan.Controls.Add(this.label2);
             this.grbThongTinTaiKhoan.Controls.Add(this.label5);
             this.grbThongTinTaiKhoan.Controls.Add(this.lbTenSach);
             this.grbThongTinTaiKhoan.Controls.Add(this.btnLuu);
@@ -209,33 +200,11 @@ namespace QuanLyNhaSach
             this.ckbThongKe.Text = "Thống Kê";
             this.ckbThongKe.UseVisualStyleBackColor = true;
             // 
-            // pnMKM
-            // 
-            this.pnMKM.BackColor = System.Drawing.Color.Black;
-            this.pnMKM.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pnMKM.Location = new System.Drawing.Point(273, 341);
-            this.pnMKM.Margin = new System.Windows.Forms.Padding(2);
-            this.pnMKM.Name = "pnMKM";
-            this.pnMKM.Size = new System.Drawing.Size(390, 1);
-            this.pnMKM.TabIndex = 38;
-            // 
-            // txbMatKhauMoi
-            // 
-            this.txbMatKhauMoi.BackColor = System.Drawing.Color.White;
-            this.txbMatKhauMoi.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txbMatKhauMoi.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbMatKhauMoi.Location = new System.Drawing.Point(274, 308);
-            this.txbMatKhauMoi.Margin = new System.Windows.Forms.Padding(2);
-            this.txbMatKhauMoi.Name = "txbMatKhauMoi";
-            this.txbMatKhauMoi.Size = new System.Drawing.Size(390, 32);
-            this.txbMatKhauMoi.TabIndex = 4;
-            this.txbMatKhauMoi.UseSystemPasswordChar = true;
-            // 
             // pnNL
             // 
             this.pnNL.BackColor = System.Drawing.Color.Black;
             this.pnNL.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pnNL.Location = new System.Drawing.Point(275, 411);
+            this.pnNL.Location = new System.Drawing.Point(226, 116);
             this.pnNL.Margin = new System.Windows.Forms.Padding(2);
             this.pnNL.Name = "pnNL";
             this.pnNL.Size = new System.Drawing.Size(390, 1);
@@ -246,7 +215,7 @@ namespace QuanLyNhaSach
             this.txbNhapLaiMKM.BackColor = System.Drawing.Color.White;
             this.txbNhapLaiMKM.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txbNhapLaiMKM.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbNhapLaiMKM.Location = new System.Drawing.Point(277, 378);
+            this.txbNhapLaiMKM.Location = new System.Drawing.Point(228, 83);
             this.txbNhapLaiMKM.Margin = new System.Windows.Forms.Padding(2);
             this.txbNhapLaiMKM.Name = "txbNhapLaiMKM";
             this.txbNhapLaiMKM.Size = new System.Drawing.Size(390, 32);
@@ -257,7 +226,7 @@ namespace QuanLyNhaSach
             // 
             this.panel3.BackColor = System.Drawing.Color.Black;
             this.panel3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel3.Location = new System.Drawing.Point(273, 271);
+            this.panel3.Location = new System.Drawing.Point(226, 51);
             this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(390, 1);
@@ -267,7 +236,7 @@ namespace QuanLyNhaSach
             // 
             this.panel7.BackColor = System.Drawing.Color.Black;
             this.panel7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel7.Location = new System.Drawing.Point(271, 201);
+            this.panel7.Location = new System.Drawing.Point(275, 219);
             this.panel7.Margin = new System.Windows.Forms.Padding(2);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(390, 1);
@@ -278,7 +247,7 @@ namespace QuanLyNhaSach
             this.txbMatKhau.BackColor = System.Drawing.Color.White;
             this.txbMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txbMatKhau.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbMatKhau.Location = new System.Drawing.Point(275, 237);
+            this.txbMatKhau.Location = new System.Drawing.Point(228, 17);
             this.txbMatKhau.Margin = new System.Windows.Forms.Padding(2);
             this.txbMatKhau.Name = "txbMatKhau";
             this.txbMatKhau.Size = new System.Drawing.Size(390, 32);
@@ -290,9 +259,10 @@ namespace QuanLyNhaSach
             this.txbTenHT.BackColor = System.Drawing.Color.White;
             this.txbTenHT.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txbTenHT.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTenHT.Location = new System.Drawing.Point(273, 168);
+            this.txbTenHT.Location = new System.Drawing.Point(277, 186);
             this.txbTenHT.Margin = new System.Windows.Forms.Padding(2);
             this.txbTenHT.Name = "txbTenHT";
+            this.txbTenHT.ReadOnly = true;
             this.txbTenHT.Size = new System.Drawing.Size(390, 32);
             this.txbTenHT.TabIndex = 2;
             // 
@@ -300,7 +270,7 @@ namespace QuanLyNhaSach
             // 
             this.panel8.BackColor = System.Drawing.Color.Black;
             this.panel8.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel8.Location = new System.Drawing.Point(273, 126);
+            this.panel8.Location = new System.Drawing.Point(277, 144);
             this.panel8.Margin = new System.Windows.Forms.Padding(2);
             this.panel8.Name = "panel8";
             this.panel8.Size = new System.Drawing.Size(390, 1);
@@ -311,7 +281,7 @@ namespace QuanLyNhaSach
             this.txbTenDN.BackColor = System.Drawing.Color.White;
             this.txbTenDN.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txbTenDN.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTenDN.Location = new System.Drawing.Point(275, 93);
+            this.txbTenDN.Location = new System.Drawing.Point(279, 111);
             this.txbTenDN.Margin = new System.Windows.Forms.Padding(2);
             this.txbTenDN.Name = "txbTenDN";
             this.txbTenDN.ReadOnly = true;
@@ -322,7 +292,7 @@ namespace QuanLyNhaSach
             // 
             this.lbNL.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbNL.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.lbNL.Location = new System.Drawing.Point(37, 371);
+            this.lbNL.Location = new System.Drawing.Point(-12, 76);
             this.lbNL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbNL.Name = "lbNL";
             this.lbNL.Size = new System.Drawing.Size(234, 42);
@@ -330,23 +300,11 @@ namespace QuanLyNhaSach
             this.lbNL.Text = "Nhập lại:";
             this.lbNL.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // lbMKM
-            // 
-            this.lbMKM.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbMKM.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.lbMKM.Location = new System.Drawing.Point(35, 301);
-            this.lbMKM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lbMKM.Name = "lbMKM";
-            this.lbMKM.Size = new System.Drawing.Size(234, 42);
-            this.lbMKM.TabIndex = 34;
-            this.lbMKM.Text = "Mật khẩu mới:";
-            this.lbMKM.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.label2.Location = new System.Drawing.Point(35, 230);
+            this.label2.Location = new System.Drawing.Point(-12, 10);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(234, 42);
@@ -358,7 +316,7 @@ namespace QuanLyNhaSach
             // 
             this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.label5.Location = new System.Drawing.Point(33, 161);
+            this.label5.Location = new System.Drawing.Point(37, 179);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(234, 42);
@@ -370,7 +328,7 @@ namespace QuanLyNhaSach
             // 
             this.lbTenSach.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbTenSach.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.lbTenSach.Location = new System.Drawing.Point(35, 86);
+            this.lbTenSach.Location = new System.Drawing.Point(39, 104);
             this.lbTenSach.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbTenSach.Name = "lbTenSach";
             this.lbTenSach.Size = new System.Drawing.Size(234, 42);
@@ -385,7 +343,7 @@ namespace QuanLyNhaSach
             this.btnLuu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLuu.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLuu.ForeColor = System.Drawing.Color.Black;
-            this.btnLuu.Location = new System.Drawing.Point(106, 493);
+            this.btnLuu.Location = new System.Drawing.Point(470, 477);
             this.btnLuu.Name = "btnLuu";
             this.btnLuu.Size = new System.Drawing.Size(195, 47);
             this.btnLuu.TabIndex = 6;
@@ -467,6 +425,19 @@ namespace QuanLyNhaSach
             this.panel1.Size = new System.Drawing.Size(1000, 50);
             this.panel1.TabIndex = 2;
             // 
+            // pnMK
+            // 
+            this.pnMK.Controls.Add(this.panel3);
+            this.pnMK.Controls.Add(this.label2);
+            this.pnMK.Controls.Add(this.pnNL);
+            this.pnMK.Controls.Add(this.txbNhapLaiMKM);
+            this.pnMK.Controls.Add(this.txbMatKhau);
+            this.pnMK.Controls.Add(this.lbNL);
+            this.pnMK.Location = new System.Drawing.Point(45, 243);
+            this.pnMK.Name = "pnMK";
+            this.pnMK.Size = new System.Drawing.Size(676, 168);
+            this.pnMK.TabIndex = 44;
+            // 
             // UCCaiDat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -486,6 +457,8 @@ namespace QuanLyNhaSach
             ((System.ComponentModel.ISupportInitialize)(this.pbDangXuat)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.pnMK.ResumeLayout(false);
+            this.pnMK.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -506,8 +479,6 @@ namespace QuanLyNhaSach
         private System.Windows.Forms.CheckBox ckbKeSach;
         private System.Windows.Forms.CheckBox ckbNhapSach;
         private System.Windows.Forms.CheckBox ckbThongKe;
-        private System.Windows.Forms.Panel pnMKM;
-        private System.Windows.Forms.TextBox txbMatKhauMoi;
         private System.Windows.Forms.Panel pnNL;
         private System.Windows.Forms.TextBox txbNhapLaiMKM;
         private System.Windows.Forms.Panel panel3;
@@ -517,12 +488,12 @@ namespace QuanLyNhaSach
         private System.Windows.Forms.Panel panel8;
         private System.Windows.Forms.TextBox txbTenDN;
         private System.Windows.Forms.Label lbNL;
-        private System.Windows.Forms.Label lbMKM;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label lbTenSach;
         private System.Windows.Forms.CheckBox ckbTaiKhoan;
         private System.Windows.Forms.CheckBox ckbBanSach;
         private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Panel pnMK;
     }
 }
--- QuanLyNhaSach/QuanLyNhaSach/UCCaiDat.cs
+++ QuanLyNhaSach/QuanLyNhaSach/UCCaiDat.cs
@@ -60,10 +60,8 @@ namespace QuanLyNhaSach
 
         private void cbTaiKhoan_SelectedIndexChanged(object sender, EventArgs e)
         {
+            pnMK.Visible = false;
             txbTenDN.ReadOnly = true;
-            lbMKM.Visible = true;
-            txbMatKhauMoi.Visible = true;
-            pnMKM.Visible = true;
             btnLuu.Text = "LƯU";
             Account account = AccountDAO.Instance.LayTaiKhoanTuTenDN(cbTaiKhoan.SelectedItem.ToString());
             txbTenDN.Text = account.TenDN;
@@ -81,16 +79,13 @@ namespace QuanLyNhaSach
         {
             if (btnLuu.Text == "LƯU")
             {
-                if (txbMatKhauMoi.Text == txbNhapLaiMKM.Text)
-                {
-                    if (txbMatKhauMoi.Text == "") txbMatKhauMoi.Text = txbMatKhau.Text;
                     int bansach = ckbBanSach.Checked == true ? 1 : 0;
                     int kesach = ckbKeSach.Checked == true ? 1 : 0;
                     int nhapsach = ckbNhapSach.Checked == true ? 1 : 0;
                     int tttaikhoan = ckbTaiKhoan.Checked == true ? 1 : 0;
                     int themdl = ckbThemDL.Checked == true ? 1 : 0;
                     int thongke = ckbThongKe.Checked == true ? 1 : 0;
-                    DataProvider.Instance.ExecuteNonQuery("EXEC USP_CapNhatTaiKhoan @tenDN , @tenHT , @nhapsach , @thongke , @kesach , @themdl , @bansach , @tttaikhoan , @mkmoi", new object[] { txbTenDN.Text, txbTenHT.Text, nhapsach, thongke, kesach, themdl, bansach, tttaikhoan, txbMatKhauMoi.Text });
+                    DataProvider.Instance.ExecuteNonQuery("EXEC USP_CapNhatTaiKhoan @tendn , @nhapsach , @thongke , @kesach , @themdl , @bansach , @tttaikhoan", new object[] { txbTenDN.Text , nhapsach, thongke, kesach, themdl, bansach, tttaikhoan});
                     DuaThongDiep(string.Concat("Đã lưu cài đặt cho tài khoản ", cbTaiKhoan.SelectedItem.ToString()), 1);
                     Account account = AccountDAO.Instance.LayTaiKhoanTuTenDN(cbTaiKhoan.SelectedItem.ToString());
                     txbTenDN.Text = account.TenDN;
@@ -102,12 +97,6 @@ namespace QuanLyNhaSach
                     ckbTaiKhoan.Checked = (account.TtTaiKhoan == 1);
                     ckbThemDL.Checked = (account.ThemDuLieu == 1);
                     ckbThongKe.Checked = (account.ThongKe == 1);
-                    txbMatKhauMoi.Text = txbNhapLaiMKM.Text = "";
-                }
-                else
-                {
-                    DuaThongDiep("Vui lòng nhập mật khẩu mới và xác nhận mật khẩu giống nhau!", 2);
-                }
             }
             else
             {
@@ -135,18 +124,16 @@ namespace QuanLyNhaSach
 
         void TrangThaiNhap()
         {
-            txbTenDN.Text = txbTenHT.Text = txbMatKhau.Text = txbMatKhauMoi.Text = txbNhapLaiMKM.Text = "";
+            txbTenDN.Text = txbTenHT.Text = txbMatKhau.Text = txbNhapLaiMKM.Text = "";
             ckbBanSach.Checked = ckbTaiKhoan.Checked = ckbKeSach.Checked = true;
             ckbThemDL.Checked = ckbThongKe.Checked = ckbNhapSach.Checked = false;
-            lbMKM.Visible = false;
-            txbMatKhauMoi.Visible = false;
-            pnMKM.Visible = false;
             txbTenDN.Focus();
         }
         private void btnThemTaiKhoan_Click(object sender, EventArgs e)
         {
             btnLuu.Text = "Thêm Tài Khoản";
             txbTenDN.ReadOnly = false;
+            pnMK.Visible = true;
             TrangThaiNhap();
         }
 
