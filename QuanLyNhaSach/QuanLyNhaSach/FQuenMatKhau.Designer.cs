d84e3503412052ba6b428c4d8df90bdb1c05f15b
Cảnh Trần Hữu <18520514@ms.uit.edu.vn>
2020-12-21 13:50:32 +0700

    Update QuenMatKhau

---
+++ QuanLyNhaSach/QuanLyNhaSach/FQuenMatKhau.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace QuanLyNhaSach
+{
+    partial class FQuenMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FQuenMatKhau));
+            this.pnMa = new System.Windows.Forms.Panel();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.lbHoTro = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.txbMa = new System.Windows.Forms.TextBox();
+            this.lbMa = new System.Windows.Forms.Label();
+            this.pnNewPassword = new System.Windows.Forms.Panel();
+            this.btnXNMK = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnNL = new System.Windows.Forms.Panel();
+            this.txbXacNhanMK = new System.Windows.Forms.TextBox();
+            this.txbMatKhau = new System.Windows.Forms.TextBox();
+            this.lbNL = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnMa.SuspendLayout();
+            this.pnNewPassword.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnMa
+            // 
+            this.pnMa.Controls.Add(this.btnLuu);
+            this.pnMa.Controls.Add(this.lbHoTro);
+            this.pnMa.Controls.Add(this.panel8);
+            this.pnMa.Controls.Add(this.txbMa);
+            this.pnMa.Controls.Add(this.lbMa);
+            this.pnMa.Location = new System.Drawing.Point(56, 313);
+            this.pnMa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnMa.Name = "pnMa";
+            this.pnMa.Size = new System.Drawing.Size(825, 292);
+            this.pnMa.TabIndex = 14;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnLuu.FlatAppearance.BorderSize = 0;
+            this.btnLuu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLuu.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.ForeColor = System.Drawing.Color.Black;
+            this.btnLuu.Location = new System.Drawing.Point(276, 209);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(260, 58);
+            this.btnLuu.TabIndex = 47;
+            this.btnLuu.Text = "Xác Nhận";
+            this.btnLuu.UseVisualStyleBackColor = false;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // lbHoTro
+            // 
+            this.lbHoTro.AutoSize = true;
+            this.lbHoTro.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHoTro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.lbHoTro.Location = new System.Drawing.Point(169, 16);
+            this.lbHoTro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbHoTro.Name = "lbHoTro";
+            this.lbHoTro.Size = new System.Drawing.Size(441, 41);
+            this.lbHoTro.TabIndex = 46;
+            this.lbHoTro.Text = "Đừng lo lắng nếu bạn có mã!";
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Black;
+            this.panel8.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel8.Location = new System.Drawing.Point(148, 176);
+            this.panel8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(520, 1);
+            this.panel8.TabIndex = 45;
+            // 
+            // txbMa
+            // 
+            this.txbMa.BackColor = System.Drawing.Color.White;
+            this.txbMa.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbMa.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMa.Location = new System.Drawing.Point(151, 135);
+            this.txbMa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txbMa.Name = "txbMa";
+            this.txbMa.Size = new System.Drawing.Size(520, 40);
+            this.txbMa.TabIndex = 43;
+            // 
+            // lbMa
+            // 
+            this.lbMa.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.lbMa.Location = new System.Drawing.Point(41, 129);
+            this.lbMa.Name = "lbMa";
+            this.lbMa.Size = new System.Drawing.Size(83, 52);
+            this.lbMa.TabIndex = 44;
+            this.lbMa.Text = "Mã:";
+            this.lbMa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pnNewPassword
+            // 
+            this.pnNewPassword.Controls.Add(this.btnXNMK);
+            this.pnNewPassword.Controls.Add(this.panel3);
+            this.pnNewPassword.Controls.Add(this.label2);
+            this.pnNewPassword.Controls.Add(this.pnNL);
+            this.pnNewPassword.Controls.Add(this.txbXacNhanMK);
+            this.pnNewPassword.Controls.Add(this.txbMatKhau);
+            this.pnNewPassword.Controls.Add(this.lbNL);
+            this.pnNewPassword.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnNewPassword.Enabled = false;
+            this.pnNewPassword.Location = new System.Drawing.Point(0, 756);
+            this.pnNewPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnNewPassword.Name = "pnNewPassword";
+            this.pnNewPassword.Size = new System.Drawing.Size(1190, 369);
+            this.pnNewPassword.TabIndex = 13;
+            // 
+            // btnXNMK
+            // 
+            this.btnXNMK.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnXNMK.FlatAppearance.BorderSize = 0;
+            this.btnXNMK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXNMK.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXNMK.ForeColor = System.Drawing.Color.Black;
+            this.btnXNMK.Location = new System.Drawing.Point(332, 223);
+            this.btnXNMK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnXNMK.Name = "btnXNMK";
+            this.btnXNMK.Size = new System.Drawing.Size(260, 58);
+            this.btnXNMK.TabIndex = 47;
+            this.btnXNMK.Text = "Xác Nhận";
+            this.btnXNMK.UseVisualStyleBackColor = false;
+            this.btnXNMK.Click += new System.EventHandler(this.btnXNMK_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(409, 96);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(416, 1);
+            this.panel3.TabIndex = 46;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.label2.Location = new System.Drawing.Point(81, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 52);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Mật khẩu Mới:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pnNL
+            // 
+            this.pnNL.BackColor = System.Drawing.Color.Black;
+            this.pnNL.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnNL.Location = new System.Drawing.Point(412, 181);
+            this.pnNL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnNL.Name = "pnNL";
+            this.pnNL.Size = new System.Drawing.Size(416, 1);
+            this.pnNL.TabIndex = 45;
+            // 
+            // txbXacNhanMK
+            // 
+            this.txbXacNhanMK.BackColor = System.Drawing.Color.White;
+            this.txbXacNhanMK.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbXacNhanMK.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbXacNhanMK.Location = new System.Drawing.Point(415, 140);
+            this.txbXacNhanMK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txbXacNhanMK.Name = "txbXacNhanMK";
+            this.txbXacNhanMK.Size = new System.Drawing.Size(416, 40);
+            this.txbXacNhanMK.TabIndex = 42;
+            this.txbXacNhanMK.UseSystemPasswordChar = true;
+            // 
+            // txbMatKhau
+            // 
+            this.txbMatKhau.BackColor = System.Drawing.Color.White;
+            this.txbMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbMatKhau.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMatKhau.Location = new System.Drawing.Point(412, 54);
+            this.txbMatKhau.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txbMatKhau.Name = "txbMatKhau";
+            this.txbMatKhau.Size = new System.Drawing.Size(416, 40);
+            this.txbMatKhau.TabIndex = 41;
+            this.txbMatKhau.UseSystemPasswordChar = true;
+            // 
+            // lbNL
+            // 
+            this.lbNL.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNL.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.lbNL.Location = new System.Drawing.Point(19, 128);
+            this.lbNL.Name = "lbNL";
+            this.lbNL.Size = new System.Drawing.Size(355, 52);
+            this.lbNL.TabIndex = 44;
+            this.lbNL.Text = "Xác Nhận Mật Khẩu:";
+            this.lbNL.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(360, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 224);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FForgotPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 901);
+            this.Controls.Add(this.pnMa);
+            this.Controls.Add(this.pnNewPassword);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FForgotPassword";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quên Mật Khẩu";
+            this.pnMa.ResumeLayout(false);
+            this.pnMa.PerformLayout();
+            this.pnNewPassword.ResumeLayout(false);
+            this.pnNewPassword.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnMa;
+        private System.Windows.Forms.Panel pnNewPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.TextBox txbMa;
+        private System.Windows.Forms.Label lbMa;
+        private System.Windows.Forms.Label lbHoTro;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pnNL;
+        private System.Windows.Forms.TextBox txbXacNhanMK;
+        private System.Windows.Forms.TextBox txbMatKhau;
+        private System.Windows.Forms.Label lbNL;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXNMK;
+    }
+}
